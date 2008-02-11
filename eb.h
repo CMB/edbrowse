de7b90291238955ee7327ac63c6c63e2ec76b547
eklhad <eklhad>
2008-02-11 19:43:59 +0000

    intFlag should be volatile

--- eb.h
+++ eb.h
@@ -187,7 +187,7 @@ extern bool unformatMail;	/* suppress formatting */
 extern bool passMail;		/* pass mail across the filters */
 extern bool errorExit;		/* exit on any error, for scripting purposes */
 extern bool isInteractive;
-extern bool intFlag;		/* set this when interrupt signal is caught */
+extern volatile bool intFlag;		/* set this when interrupt signal is caught */
 extern bool binaryDetect;
 extern bool inInput;		/* reading line from standard in */
 extern int fileSize;		/* when reading/writing files */
