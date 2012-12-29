e2a12530aaf15944fb64579c9fc064badc111ff7
Christopher Brannon <chris@the-brannons.com>
2012-12-29 06:22:58 -0800

    Version 3.4.9.

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 /* See eb.h for descriptive comments. */
 
 
-const char *version = "3.4.8";
+const char *version = "3.4.9";
 char *userAgents[10], *currentAgent, *currentReferrer;
 const char eol[] = "\r\n";
 char EMPTYSTRING[] = "";
