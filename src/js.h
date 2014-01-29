83e457001c3fed74ecf3dd6c8b447c1497b31534
Karl Dahlke <eklhad@comcast.net>
2014-01-29 12:40:11 -0500

    Get caught up on some documentation, reflecting the work we are doing.
    Especially README and sample.ebrc, which were many versions out of date.
    Credit to Adam in the users guide, and finally get Cleverson's name
    and email right.
    CHANGES describe the changes for the next version.

--- src/js.h
+++ src/js.h
@@ -1,7 +1,7 @@
 /* js.h, a laywer between edbrowse and the javascript engine.
  * This is a form of encapsulation, and it has proven to be vital,
  * since Mozilla js changes quite often, and those changes
- * can be combined to this file, jsdom.c and jsloc.c.
+ * can be comfined to jsdom.cpp, jsloc.cpp, and html.cpp.
  * No other file need know about the javascript API,
  * and no other file should include jsapi.h. */
 
