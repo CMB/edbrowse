8497c708fb352c9e44fe82d0e5b0940e87d57f52
Karl Dahlke <eklhad@comcast.net>
2014-12-14 08:04:46 -0500

    Add hello world program for v8, though it doesn't compile. Maybe someone else can tweak it.
    Also some hello targets in the makefile.

---
+++ src/js_hello_v8.cpp
@@ -0,0 +1,49 @@
+#include <v8.h>
+#include <libplatform/libplatform.h>
+
+using namespace v8;
+
+static const char runCommand[] = "'hello world, the answer is ' + 6*7;";
+
+int main(int argc, char* argv[]) {
+  // Initialize V8.
+  V8::InitializeICU();
+  Platform* platform = platform::CreateDefaultPlatform();
+  V8::InitializePlatform(platform);
+  V8::Initialize();
+
+  // Create a new Isolate and make it the current one.
+  Isolate* isolate = Isolate::New();
+  {
+    Isolate::Scope isolate_scope(isolate);
+
+    // Create a stack-allocated handle scope.
+    HandleScope handle_scope(isolate);
+
+    // Create a new context.
+    Local<Context> context = Context::New(isolate);
+
+    // Enter the context for compiling and running the hello world script.
+    Context::Scope context_scope(context);
+
+    // Create a string containing the JavaScript source code.
+    Local<String> source = String::NewFromUtf8(isolate, runCommand);
+
+    // Compile the source code.
+    Local<Script> script = Script::Compile(source);
+
+    // Run the script to get the result.
+    Local<Value> result = script->Run();
+
+    // Convert the result to an UTF8 string and print it.
+    String::Utf8Value utf8(result);
+    printf("%s\n", *utf8);
+  }
+
+  // Dispose the isolate and tear down V8.
+  isolate->Dispose();
+  V8::Dispose();
+  V8::ShutdownPlatform();
+  delete platform;
+  return 0;
+}
