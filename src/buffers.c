88913aff97528d8a905c644f89d24c64505bbe7b
Karl Dahlke <eklhad@comcast.net>
2015-05-07 08:20:28 -0400

    Fix the bug caused by making a change then issuing the capital M command.

--- src/buffers.c
+++ src/buffers.c
@@ -4017,6 +4017,9 @@ bool runCommand(const char *line)
 			return false;
 		if (cxActive(cx) && !cxQuit(cx, 2))
 			return false;
+/* If changes were made to this buffer, they are undoable after the move */
+		undoCompare();
+		cw->undoable = false;
 /* Magic with pointers, hang on to your hat. */
 		sessionList[cx].fw = sessionList[cx].lw = cw;
 		cs->lw = cw->prev;
