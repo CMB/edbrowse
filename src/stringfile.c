438f86556d8fb67d9938a70454194ffa8656a230
Karl Dahlke <eklhad@comcast.net>
2015-09-23 23:39:30 -0400

    Print date in big endian, standard, international, sortable, but still not as quick to read as I would like.

--- src/stringfile.c
+++ src/stringfile.c
@@ -924,8 +924,8 @@ char *conciseTime(time_t t)
 {
 	static char buffer[20];
 	struct tm *tm = localtime(&t);
-	sprintf(buffer, "%02d/%02d/%04d %02d:%02d",
-		tm->tm_mon + 1, tm->tm_mday, tm->tm_year + 1900,
+	sprintf(buffer, "%04d-%02d-%02d %02d:%02d",
+		tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
 		tm->tm_hour, tm->tm_min);
 	return buffer;
 }				/* conciseTime */
