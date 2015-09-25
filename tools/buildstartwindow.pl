3e2b048c8fc556e3e4c32b4349cc325b8a4935c5
Karl Dahlke <eklhad@comcast.net>
2015-09-25 15:24:25 -0400

    Use a perl script rather than a shell script to turn startwindow.js into startwindow.c. This is step 1 in porting to windows; no shell scripts in windows.

---
+++ tools/buildstartwindow.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -w
+#  turn startwindow.js into startwindow.c
+# Windows has a limit of 16380 single-byte characters.
+use strict;
+use warnings;
+
+my $infile = "startwindow.js";
+my $outfile = "startwindow.c";
+
+my $max_chars = 16380;
+
+sub prt($) { print shift; }
+
+if ( -f $infile ) {
+    if (!open  INF, "<$infile") {
+        prt("Error: Unable to open $infile file!\n");
+        exit(1);
+    }
+    my @lines = <INF>;
+    close INF;
+    if (! open OUTF, ">$outfile") {
+        prt("Error: Unable to create $outfile file!\n");
+        exit(1);
+    }
+    print OUTF "/* startwindow.c: this file is machine generated; */\n";
+    print OUTF "/* please edit startwindow.js instead. */\n";
+    print OUTF "\n";
+    print OUTF "const char startWindowJS[] = \"\\\n";
+    my ($line,$len,$total);
+    $total = 0;
+    foreach $line (@lines) {
+        chomp $line;
+        $line =~ s/\\/\\\\/g;
+        $line =~ s/"/\\"/g;
+        $len = length($line) + 4;
+        if (($total + $len) > $max_chars) {
+            print OUTF "\"\n\"";
+            $total = 0;
+        }
+        print OUTF "$line\\n\\\n";
+        $total += $len + 4;
+    }
+    print OUTF "\";\n";
+    print OUTF "\n";
+    close OUTF;
+    prt("Content $infile written to $outfile\n");
+    exit(0);    
+} else {
+    prt("Error: Unable to locate $infile file!\n");
+    exit(1);
+}
+
+# eof
+
