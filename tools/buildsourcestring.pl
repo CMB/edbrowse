88b2e55658d14052f49def7d501f5b6cc3dc31ea
Karl Dahlke <eklhad@comcast.net>
2015-10-02 04:15:02 -0400

    Allow for multiple files to be converted into C strings in one output file. This feature is not used yet. It's for internationalization in the future.

--- tools/buildsourcestring.pl
+++ tools/buildsourcestring.pl
@@ -6,20 +6,30 @@ use warnings;
 
 sub prt($) { print shift; }
 
-if($#ARGV != 2) {
-prt "Usage: buildsourcestring.pl infile outfile stringname\n";
+my $max_chars = 16380;
+
+my $nargs = $#ARGV;
+if($nargs < 2) {
+prt "Usage: buildsourcestring.pl file1 string1 file2 string2 ... outfile\n";
 exit 1;
 }
 
-my $infile = $ARGV[0];
-my $outfile = $ARGV[1];
-my $stringname = $ARGV[2];
-my $inbase = $infile;
-$inbase =~ s,.*/,,;
+my $outfile = $ARGV[$nargs];
 my $outbase = $outfile;
 $outbase =~ s,.*/,,;
 
-my $max_chars = 16380;
+    if (! open OUTF, ">$outfile") {
+        prt("Error: Unable to create $outfile file!\n");
+        exit(1);
+    }
+    print OUTF "/* $outbase: this file is machine generated; */\n\n";
+
+#  loop over input files
+for(my $j = 0; $j < $nargs; $j += 2) {
+my $infile = $ARGV[$j];
+my $inbase = $infile;
+$inbase =~ s,.*/,,;
+my $stringname = $ARGV[$j+1];
 
 if ( -f $infile ) {
     if (!open  INF, "<$infile") {
@@ -28,13 +38,7 @@ if ( -f $infile ) {
     }
     my @lines = <INF>;
     close INF;
-    if (! open OUTF, ">$outfile") {
-        prt("Error: Unable to create $outfile file!\n");
-        exit(1);
-    }
-    print OUTF "/* $outbase: this file is machine generated; */\n";
-    print OUTF "/* please edit $inbase instead. */\n";
-    print OUTF "\n";
+    print OUTF "/* source file $inbase */\n";
     print OUTF "const char *$stringname = \"\\\n";
     my ($line,$len,$total);
     $total = 0;
@@ -52,13 +56,15 @@ if ( -f $infile ) {
     }
     print OUTF "\";\n";
     print OUTF "\n";
-    close OUTF;
     prt("Content $infile written to $outfile\n");
-    exit(0);    
 } else {
     prt("Error: Unable to locate $infile file!\n");
     exit(1);
 }
+}
+
+close OUTF;
+exit(0);    
 
 # eof
 
