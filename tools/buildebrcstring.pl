af8e1ceb678dd255d40f1eaea9374485ec152cbb
Karl Dahlke <eklhad@comcast.net>
2015-10-12 02:16:45 -0400

    Build ebrc.c from all the available ebrc language files.

---
+++ tools/buildebrcstring.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+# Wrapper around buildsourcestring.pl, for the ebrc files in various languages.
+
+use strict;
+use warnings;
+
+my $s = join ' ', glob "doc/ebrc-*";
+$s =~ s/ebrc-\w+/$& $&/g;
+# Some inconsistency has evolved here,
+# file name is hyphen, but C string has to be an underscore.
+$s =~ s/ ebrc-/ ebrc_/g;
+
+# Ready to go.
+system "perl -w tools/buildsourcestring.pl $s src/ebrc.c";
+
+exit 0;
