f76c73f77dfb9cae14c17bca959cd620f34dae56
Geoff McLane <ubuntu@geoffair.info>
2016-01-10 13:28:41 +0100

    Add __cplusplus extern C macro

--- win32/vsprtf.h
+++ win32/vsprtf.h
@@ -8,10 +8,15 @@
 
 #ifndef _VSPRTF_HXX_
 #define _VSPRTF_HXX_
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern int vasprintf (char **str, const char *fmt, va_list args);
 extern int asprintf (char **str, const char *fmt, ...);
 
-
+#ifdef __cplusplus
+}
+#endif
 #endif // #ifndef _VSPRTF_HXX_
 // eof - vsprtf.hxx
