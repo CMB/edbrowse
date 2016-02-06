d2455ec0e106ce1bbc68380bcdb976c037ddd036
Adam Thompson <arthompson1990@gmail.com>
2016-02-06 11:48:31 +0000

    Tidy up build-me.sh - replace if ... elif... else construct with case statement - fix various quoting issues - re-indent to edbrowse standards (four space indents rather than tabs) - allow the use of the CMAKE_INSTALL_PREFIX environment variable - alter command line parsing to use correct quoting (shell default for loop)

--- build/build-me.sh
+++ build/build-me.sh
@@ -1,51 +1,43 @@
 #!/bin/sh
 #< build-me.sh - 20150925 - for edbrowse project
-BN=`basename $0`
-
+BN="`basename $0`"
 BLDLOG="bldlog-1.txt"
 
 if [ -f "$BLDLOG" ]; then
-	rm -f $BLDLOG
+    rm -f "$BLDLOG"
 fi
 
 ##############################################
 ### ***** NOTE THIS INSTALL LOCATION ***** ###
 ### Change to suit your taste, environment ###
 ##############################################
-TMPOPTS="-DCMAKE_INSTALL_PREFIX=$HOME"
+: ${TMPOPTS:="-DCMAKE_INSTALL_PREFIX=\"${CMAKE_INSTALL_PREFIX:-$HOME}\""}
 #############################################
 # Use -DCMAKE_BUILD_TYPE=Debug to add gdb symbols
 # Use -DCMAKE_VERBOSE_MAKEFILE=ON
 
-for arg in $@; do
-	if [ "$arg" = "VERBOSE" ]; then
-		TMPOPTS="$TMPOPTS -DCMAKE_VERBOSE_MAKEFILE=ON"
-	elif [ "$arg" = "DEBUG" ]; then
-		TMPOPTS="$TMPOPTS -DCMAKE_BUILD_TYPE=Debug"
-	else
-		TMPOPTS="$TMPOPTS $arg"
-	fi
+# correctly quoted $@ is the default for shell for loops
+for arg; do
+    case "$arg" in
+       VERBOSE) TMPOPTS="$TMPOPTS -DCMAKE_VERBOSE_MAKEFILE=ON";;
+        DEBUG) TMPOPTS="$TMPOPTS -DCMAKE_BUILD_TYPE=Debug";;
+        *) TMPOPTS="$TMPOPTS $arg";;
+    esac
 done
 
 echo "$BN: Doing: 'cmake .. $TMPOPTS' to $BLDLOG"
-cmake .. $TMPOPTS >> $BLDLOG 2>&1
-if [ ! "$?" = "0" ]; then
-	echo "$BN: cmake confiuration, generation error"
-	exit 1
+if ! eval cmake .. $TMPOPTS >> $BLDLOG 2>&1; then
+    echo "$BN: cmake confiuration, generation error"
+    exit 1
 fi
 
 echo "$BN: Doing: 'make' to $BLDLOG"
-make >> $BLDLOG 2>&1
-if [ ! "$?" = "0" ]; then
-	echo "$BN: make error - see $BLDLOG for details"
-	exit 1
+if ! make >> $BLDLOG 2>&1; then
+    echo "$BN: make error - see $BLDLOG for details"
+    exit 1
 fi
 
 echo ""
 echo "$BN: appears a successful build... see $BLDLOG for details"
 echo ""
-echo "$BN: Time for 'make install' IFF desired... to $HOME/bin, unless changed..."
-echo ""
-
-# eof
-
+echo "$BN: Time for 'make install' IF desired... to ${CMAKE_INSTALL_PREFIX:-$HOME}/bin"
