65dcc4d5d212f96f75b134c282678edcb6f36b98
Karl Dahlke <eklhad@comcast.net>
2018-03-29 21:25:41 -0400

    Version.

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@
 
 const char *progname;
 const char eol[] = "\r\n";
-const char *version = "3.7.2";
+const char *version = "3.7.3";
 char *changeFileName;
 char *configFile, *addressFile, *cookieFile;
 char *mailDir, *mailUnread, *mailStash, *mailReply;
