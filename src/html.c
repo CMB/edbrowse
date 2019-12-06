b2987255966d51176762d01f7915242e71d4aee8
Alfonso S. Siciliano <alfix86@gmail.com>
2019-12-06 18:20:19 +0100

    FreeBSD has not pthread_tryjoin_np()
    FreeBSD has not pthread_tryjoin_np()

--- src/html.c
+++ src/html.c
@@ -12,9 +12,9 @@ extern int gettimeofday(struct timeval *tp, void *tzp);	// from tidys.lib
 #define SLEEP sleep
 #endif // _MSC_VER y/n
 
-// OSX has no pthread_tryjoin_np, so we can't do our
-// asynchronous timers under OSX, which is really no great loss.
-#ifdef __APPLE__
+// OSX and FreeBSD have no pthread_tryjoin_np, so we can't do our
+// asynchronous timers under OSX/FreeBSD, which is really no great loss.
+#if defined(__APPLE__) || defined(__FreeBSD__)
 #define pthread_tryjoin_np pthread_join
 #endif
 
