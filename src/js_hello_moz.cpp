237b86a7952a1d252274c7b7408971f22fb57ad8
Karl Dahlke <eklhad@comcast.net>
2020-10-30 01:41:50 -0400

    hello_moz makes three contexts, and links the first into the others under mw0.

--- src/js_hello_moz.cpp
+++ src/js_hello_moz.cpp
@@ -2,8 +2,10 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/Projects/SpiderMonkey/How_to_embed_the_JavaScript_engine
 // I tweaked it for moz 60.
 
-#include "jsapi.h"
-#include "js/Initialization.h"
+#include <jsapi.h>
+#include <js/Initialization.h>
+
+#include <ctype.h>
 
 static JSClassOps global_ops = {
 // different members in different versions, so specfiy explicitly
@@ -61,8 +63,6 @@ static JSFunctionSpec nativeMethods[] = {
   JS_FS_END
 };
 
-static bool iaflag; // interactive
-
 // I don't understand any of this. Code from:
 // http://mozilla.6506.n7.nabble.com/what-is-the-replacement-of-JS-SetErrorReporter-in-spidermonkey-60-td379888.html
 // I assume all these variables are somehow on stack
@@ -88,29 +88,71 @@ JS_ClearPendingException(cx);
 }
 }
 
+static JSRuntime *jrt;		/* our js runtime environment */
+
 int main(int argc, const char *argv[])
 {
+bool iaflag; // interactive
+int ci, cl;
+JSContext *cxlist[3], *cx;
+JSObject *glist[3], *g;
+bool ok;
+const char *script, *filename;
+int lineno;
+
+if(argc > 1 && !strcmp(argv[1], "-i")) iaflag = true;
+
     JS_Init();
 
-    JSContext *cx = JS_NewContext(8L * 1024 * 1024);
+cl = iaflag ? 3 : 1;
+
+// opening a second context causes a seg fault on moz 60.
+// No clue why. For now this has to run on moz 52.
+
+for(ci=0; ci<cl; ++ci) {
+if(!ci) { // first one
+cx = JS_NewContext(JS::DefaultHeapMaxBytes);
+} else {
+// It seems not to matter whether we make independent contextts,
+// or contexts under a parent context.
+//cx = JS_NewContext(JS::DefaultHeapMaxBytes);
+cx = JS_NewContext(JS::DefaultHeapMaxBytes, JS::DefaultNurseryBytes, cxlist[0]);
+}
+cxlist[ci] = cx;
     if (!cx)
         return 1;
     if (!JS::InitSelfHostedCode(cx))
         return 1;
-
-if(argc > 1 && !strcmp(argv[1], "-i")) iaflag = true;
-
-    { // Scope for our various stack objects (JSAutoRequest, RootedObject), so they all go
-      // out of scope before we JS_DestroyContext.
-
-      JSAutoRequest ar(cx); // In practice, you would want to exit this any
-                            // time you're spinning the event loop
-
+      JSAutoRequest ar(cx);
       JS::CompartmentOptions options;
       JS::RootedObject global(cx, JS_NewGlobalObject(cx, &global_class, nullptr, JS::FireOnNewGlobalHook, options));
       if (!global)
           return 1;
+g = glist[ci] = global;
+        JSAutoCompartment ac(cx, g);
+// why can't I just pass g?
+// Why do I need a rooted object, or handle or some such?
+        JS_InitStandardClasses(cx, global);
+JS_DefineFunctions(cx, global, nativeMethods);
+// link back to master window
+if(ci) {
+JS::RootedValue mw0val(cx);
+mw0val = JS::ObjectValue(*glist[0]);
+if(!JS_DefineProperty(cx, global, "mw0", mw0val,
+(JSPROP_READONLY|JSPROP_PERMANENT))) {
+puts("unable to create mw0");
+return 1;
+}
+}
+}
 
+//  puts("after 3 loop");
+
+{
+cx = cxlist[0];
+g = glist[0];
+      JSAutoRequest ar(cx);
+        JSAutoCompartment ac(cx, g);
 //      JS::RootedValue rval(cx);
 // This works, but the GC rooting guide suggests doing it a different way,
 // which also works. I don't know if this new approach is part of 60.
@@ -120,14 +162,9 @@ if(argc > 1 && !strcmp(argv[1], "-i")) iaflag = true;
       JS::RootedValue v(cx);
       JS::MutableHandleValue rval = &v;
 
-      { // Scope for JSAutoCompartment
-        JSAutoCompartment ac(cx, global);
-        JS_InitStandardClasses(cx, global);
-JS_DefineFunctions(cx, global, nativeMethods);
-
-        const char *script = "letterInc('gdkkn')+letterDec('!xpsme') + ', it is '+new Date()";
-        const char *filename = "noname";
-        int lineno = 1;
+        script = "letterInc('gdkkn')+letterDec('!xpsme') + ', it is '+new Date()";
+        filename = "noname";
+        lineno = 1;
         JS::CompileOptions opts(cx);
         opts.setFileAndLine(filename, lineno);
 // The call to Evaluate was passed &rval when rval was the rooted value;
@@ -135,7 +172,7 @@ JS_DefineFunctions(cx, global, nativeMethods);
 // The prototype in jsapi.h expects JS::MutableHandleValue rval as fifth parameter.
 // Why then does the old way work?
 // Because JS::RootedValue * casts properly into JS::MutableHandleValue, I guess.
-        bool ok = JS::Evaluate(cx, opts, script, strlen(script), rval);
+        ok = JS::Evaluate(cx, opts, script, strlen(script), rval);
         if (!ok)
           return 1;
       JSString *str = rval.toString();
@@ -147,12 +184,34 @@ char *es = JS_EncodeString(cx, str);
 free(es);
 // The original hello program didn't free es, but who cares, exit(0),
 // thus these little sample programs sometimes leave out important details.
+}
 
 if(iaflag) {
 char line[500];
 while(fgets(line, sizeof(line), stdin)) {
 // should check for line too long here
+      JSAutoRequest ar(cx);
+        JSAutoCompartment ac(cx, g);
+      JS::RootedValue v(cx);
+      JS::MutableHandleValue rval = &v;
+JSString *str;
+char *es;
+
+// change context?
+if(line[0] == 'e' &&
+line[1] >= '1' && line[1] <= '3' &&
+isspace(line[2])) {
+printf("context %c\n", line[1]);
+cx = cxlist[line[1] - '1'];
+g = glist[line[1] - '1'];
+continue;
+}
+
 script = line;
+        filename = "noname";
+        lineno = 1;
+        JS::CompileOptions opts(cx);
+        opts.setFileAndLine(filename, lineno);
         ok = JS::Evaluate(cx, opts, script, strlen(script), rval);
 if(!ok) {
 ReportJSException(cx);
@@ -184,11 +243,13 @@ default: puts("undefined"); break;
 }
 }
 
-      }
+// closing the second context causes a seg fault
+for(ci=cl-1; ci>=0; --ci) {
+  printf("down %d\n", ci);
+    JS_DestroyContext(cxlist[ci]);
+  puts("gone");
+}
 
-    }
-
-    JS_DestroyContext(cx);
     JS_ShutDown();
     return 0;
 }
