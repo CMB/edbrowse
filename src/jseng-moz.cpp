c7df49a555e5cb27fe828ddf9eb5c7ef2167e54a
Karl Dahlke <eklhad@comcast.net>
2020-12-10 11:13:56 -0500

    More comprehensive rootchain() function, this is just for debugging.

--- src/jseng-moz.cpp
+++ src/jseng-moz.cpp
@@ -92,8 +92,7 @@ rooted objects on the heap, and then I don't have to do all this crazy hacking.
 So, rootchain() is for debugging, it steps down the chain
 and prints the first two pointers at each step, just the last 3 digites,
 then the length of the chain.
-Also the length of the rooted value chain, which is separate,
-rooted strings also a separate chain.
+The rooted value chain and rooted string chain are separate.
 jsUnroot unroots all our tag objects,
 which would otherwise never unroot and become a substantial memory leak.
 jsClose() prints an error message if we have not cleaned up all our rooted
@@ -112,40 +111,68 @@ struct cro { // chain of allocated rooted objects
 typedef struct cro Cro;
 static Cro *o_tail; // chain of globals and tags
 
+// Rooting window, to hold on to any objects that edbrowse might access.
+static JS::RootedObject *rw0;
+
 static void last3(void *v)
 {
-	unsigned long l = (unsigned long)v;
-	l %= 1000;
-	printf("%03lu", l);
+	if(!v) {
+		printf("nil");
+	} else {
+		unsigned long l = (unsigned long)v;
+		l %= 1000;
+		printf("%03lu", l);
+	}
 }
 
 // for debugging
 void rootchain(void)
 {
 	JS::RootedObject o(cxa);
+	void **top = (void**)rw0;
+	if(o_tail)
+		top = (void**)(o_tail->m);
 	void **l;
 	int n;
-	Cro *u;
+	Cro *u = 0;
 	n = 0, l = (void**)&o;
-#if 1
 	while(l) {
 last3(l[0]), printf(":"), last3(l[1]), puts("");
+if(l == (void**)rw0) puts("root");
 ++n, l = (void**)l[1];
+if(!l) break;
+if(!u) {
+if(l != top) continue;
+puts("top");
+u = o_tail;
+if(!u) { // none of our objects are present
+if(l[1]) puts("error: objects below rw0");
 }
-#else
-	while(l) ++n, l = (void**)l[1];
-#endif
+continue;
+}
+if(!u) continue;
+// make sure we all step down together
+if(!u->prev) {
+if(l != (void**)rw0)
+puts("error: objects between rw0 and our first object");
+u = 0;
+continue;
+}
+if(l == (void**)u->prev->m) {
+u = u->prev;
+continue;
+}
+puts("error: object mismatch root chain and Cro chain");
+u = 0;
+}
 	printf("chain %d|", n);
 	n = 0, u = o_tail;
 	while(u) ++n, u = u->prev;
 	printf("%d", n);
 	l = (void**)&o;
-	if(l[1] == 0 && !o_tail || l[1] == (void*)(o_tail->m))
+	if(l[1] == (void*)rw0 && !o_tail || l[1] == (void*)(o_tail->m))
 	printf(" flat");
-	JS::RootedValue v(cxa);
-	n = 0, l = (void**)&v;
-	while(l) ++n, l = (void**)l[1];
-	printf(" v %d\n", n);
+puts("");
 }
 
 // unravle the objects of our tags, our frames, if we can
@@ -170,9 +197,6 @@ void jsUnroot(void)
 		o_tail = u = v;
 	}
 }
-
-// Rooting window, to hold on to any objects that edbrowse might access.
-static JS::RootedObject *rw0;
 
 static void jsRootAndMove(void ** dest, JSObject *j, bool isglobal)
 {
