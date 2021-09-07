0733f58a353eafdae03d0d07b347d938cb7967fd
Karl Dahlke <eklhad@comcast.net>
2021-09-07 17:05:13 -0400

    Only run the snapshot adjust function on the main page, not a subframe.

--- src/jseng-quick.c
+++ src/jseng-quick.c
@@ -3944,7 +3944,7 @@ void set_basehref(const char *h)
 // This is special code for snapshot simulations.
 // If the file jslocal is present, push base over to window.location,
 // as though you were running that page.
-	if (!access("jslocal", 4) && h[0]) {
+	if (!access("jslocal", 4) && h[0] && cf == &cw->f0) {
 		run_function_bool_win(cf, "eb$base$snapshot");
 		nzFree(cf->fileName);
 		cf->fileName = cloneString(h);
