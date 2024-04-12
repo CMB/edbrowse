875a13742a9231c01dcaa74a9c56fbcb9f18c447
Adam Thompson <arthompson1990@gmail.com>
2024-04-12 19:34:36 +0100

    Improve quoting to make the helper script shellcheck clean

--- src/make-helper.sh
+++ src/make-helper.sh
@@ -8,7 +8,7 @@ fi
 [ -n "$PKG_CONFIG" ] || PKG_CONFIG=pkg-config
 
 # make sure pkg-config is installed
-if ! type $PKG_CONFIG >/dev/null 2>&1 ; then echo pkg-config-not-installed; exit 0; fi
+if ! type "$PKG_CONFIG" >/dev/null 2>&1 ; then echo pkg-config-not-installed; exit 0; fi
 
 cmd="$1"
 shift
@@ -17,11 +17,11 @@ case "${cmd}" in
         pkg_config_libs=""
         for lib in "$@";do
             pkg_config_name="${lib%:*}"
-            if $PKG_CONFIG --exists "${pkg_config_name}"; then
+            if "$PKG_CONFIG" --exists "${pkg_config_name}"; then
                 pkg_config_libs="${pkg_config_libs} ${pkg_config_name}"
             fi
         done
-        $PKG_CONFIG --cflags-only-I "${pkg_config_libs}"
+        "$PKG_CONFIG" --cflags-only-I "${pkg_config_libs}"
         ;;
 	pkg-config-libs)
         pkg_config_libs=""
@@ -29,13 +29,13 @@ case "${cmd}" in
         for lib in "$@";do
             pkg_config_name="${lib%:*}"
             lib_name="${lib#*:}"
-            if $PKG_CONFIG --exists "${pkg_config_name}"; then
+            if "$PKG_CONFIG" --exists "${pkg_config_name}"; then
                 pkg_config_libs="${pkg_config_libs} ${pkg_config_name}"
             else
                 other_libs="${other_libs} -l${lib_name}"
             fi
         done
-        printf %s\\n "$($PKG_CONFIG --libs "${pkg_config_libs}") ${other_libs}"
+        printf %s\\n "$("$PKG_CONFIG" --libs "${pkg_config_libs}") ${other_libs}"
         ;;
 
 	*)
